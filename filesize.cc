7843be59f97a7f1a8df2812eca8c0d60dacc9092
Jordan Wells <jordanwells@Jordans-MacBook-Pro.local>
2022-01-12 20:43:12 -0500

    Added functionality for counting lines

--- filesize.cc
+++ filesize.cc
@@ -10,9 +10,24 @@ using namespace std;
 
 int main( int argc, char* argv[] )
 {
+     std::cout << "program: " << argv[0] << '\n' ;
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+    for (int arg = 1; arg < argc; ++arg){
+        std::ifstream ins;
+        ins.open(argv[arg]);
+        if(ins.fail())
+            std::cout << " " << argv[arg] << ": "<< -1 << '\n' ;
+        else{
+            int count = 0;
+            std::string line;
+            while(std::getline(ins, line))
+                count++;
 
+            std::cout << " " << argv[arg] << ": "<< count << '\n' ;
+        }
+        
+    }
+    
+    
     exit(0); // this means that the program executed correctly!
 }
